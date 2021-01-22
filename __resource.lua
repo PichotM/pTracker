930cf00a636f6b25e16ce86241b6c870a281794e
Hamy-os <hamzaalisic0@gmail.com>
2021-01-22 12:42:39 +0300

    Updated files
    updates fxmanifest.lua and added data folder

--- __resource.lua
+++
@@ -1,1 +0,0 @@
-server_script 'dist/server.js'
---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'PichotM'
+description 'playtime tracker'
+version '1.0.0'
+
+server_script 'dist/server.js'
+
